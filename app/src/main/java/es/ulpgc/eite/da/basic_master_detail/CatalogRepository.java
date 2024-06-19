f1123e744afd689fe1355cfb88ab9acd9b4363d5
Luis Hernandez <lhdez@iuma.ulpgc.es>
2024-06-19 10:34:58 +0100

    cambios sin importancia

--- app/src/main/java/es/ulpgc/eite/da/basic_master_detail/CatalogRepository.java
+++ app/src/main/java/es/ulpgc/eite/da/basic_master_detail/CatalogRepository.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CatalogRepository {
+
     private static CatalogRepository instance;
     private List<CategoryItem> categories;
 
